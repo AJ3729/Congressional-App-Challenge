be1e9668a8a7d19ea9e364a4bfdf7574085c47d3
David-T34 <dave@krystal.nyc>
2025-10-15 23:48:09 -0400

    chat.py
    updated

--- tenant-aid/backend/routes/chat.py
+++ tenant-aid/backend/routes/chat.py
@@ -1,14 +1,14 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ.get("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"))
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY")
 
 def generate_ai_answer():
     user_input = input("How can I help you today? ")
 
     response = client.responses.create(
         model="gpt-4o",
-        instructions="You are a assistant that helps with people with housing questions.",
+        instructions="You are a housing assistant to tennants.",
         input=user_input,
     )
 
